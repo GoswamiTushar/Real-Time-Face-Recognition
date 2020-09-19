7f7960a4d56ca5e2f8bbd023a915ff35d25c6b6f
ardourApeX <joygupta501@gmail.com>
2020-09-20 03:50:02 +0530

    Face Recognition done!!

--- read_video.py
+++ read_video.py
@@ -5,22 +5,32 @@ import cv2 as cv
 # Capture video for indefinite period of time
 capture = cv.VideoCapture(0) 
 
+
+# haarcascade_frontalface_alt is a pre-build classifier.
+face_cascade = cv.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+
 while True:
 
 	Bool_val, frame = capture.read()
 
-	Gray_Frame =	cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-	BGR_Frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+	Gray_Frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
 	# Incase if any fault occurs
 	if Bool_val == False:
 		continue
 
-	cv.imshow("Frame", frame) #Display 
-	cv.imshow('Gray Image', Gray_Frame)
-	cv.imshow('BGR_Frame', BGR_Frame)
+	# Storing coordinates of recognized faces		
+	faces = face_cascade.detectMultiScale(Gray_Frame, 1.3, 5) #1.3 is comression ratio and 5 is no of neighbours
 
+	# Iteration over list of tuples containing (X_coordinate, Y_coordinate, width, height)
+	for (x, y, w, h) in faces:
+		cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2) #Drawing rectangle
+	
+
+	cv.imshow("Gotchaa!!!", frame) # A rectangle will be drawn over the frame
+	
+	#cv.imshow('Gray Image', Gray_Frame)
 	
 	# Wait for user input to terminate video stream
 	key_pressed = cv.waitKey(1) & 0xFF
